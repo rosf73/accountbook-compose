755ac5178057caed44ebc5bb0c6e0ea645814707
rosf73 <alswlzhsclq@naver.com>
2022-07-26 15:18:20 +0900

    [CHORE] 디자인 요구 사항에 따른 Color, Theme 정의

--- app/src/main/java/com/woowacamp/android_accountbook_15/ui/theme/Color.kt
+++ app/src/main/java/com/woowacamp/android_accountbook_15/ui/theme/Color.kt
@@ -6,3 +6,14 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val Purple = Color(0xFF524D90)
+val LightPurple = Color(0xFFA79FCB)
+val Purple04 = Color(0x66A79FCB)
+
+val OffWhite = Color(0xFFF7F6F3)
+val White = Color(0xFFFFFFFF)
+val White05 = Color(0x7FFFFFFF)
+
+val Yellow = Color(0xFFF5B853)
+val Red = Color(0xFFE75B3F)
