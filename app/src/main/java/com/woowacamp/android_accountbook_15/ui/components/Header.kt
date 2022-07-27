7be96e2f9e4e6ef4e68dab2c5d636809158e06fc
rosf73 <alswlzhsclq@naver.com>
2022-07-27 23:37:46 +0900

    [FEAT] [Main] App bar 컴포넌트로 분리하여 구현

---
+++ app/src/main/java/com/woowacamp/android_accountbook_15/ui/components/Header.kt
@@ -0,0 +1,51 @@
+package com.woowacamp.android_accountbook_15.ui.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.TabRowDefaults.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.woowacamp.android_accountbook_15.ui.theme.Purple
+
+@Composable
+fun Header(
+    modifier: Modifier = Modifier,
+    title: String,
+    leftIcon: ImageVector? = null,
+    leftIconDescription: String = "",
+    leftCallback: (() -> Unit)? = null,
+    rightIcon: ImageVector? = null,
+    rightIconDescription: String = "",
+    rightCallback: (() -> Unit)? = null
+) {
+    Row(
+        modifier = modifier
+            .padding(16.dp)
+    ) {
+        leftIcon?.let {
+            IconButton(onClick = { leftCallback!!() }) {
+                Icon(imageVector = leftIcon, contentDescription = leftIconDescription)
+            }
+        }
+        Text(
+            text = title,
+            textAlign = TextAlign.Center,
+            fontSize = 18.sp,
+            modifier = modifier
+                .weight(1f)
+        )
+        rightIcon?.let {
+            IconButton(onClick = { rightCallback!!() }) {
+                Icon(imageVector = rightIcon, contentDescription = rightIconDescription)
+            }
+        }
+    }
+    Divider(color = Purple, thickness = 1.dp)
+}
