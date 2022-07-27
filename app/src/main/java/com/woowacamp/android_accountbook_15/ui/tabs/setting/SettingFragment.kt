520384149136383545e352add46427eeba8def3c
rosf73 <alswlzhsclq@naver.com>
2022-07-27 23:39:22 +0900

    [FEAT] [Setting] 설정 App bar 적용 및 LazyColumn 임시 적용

--- app/src/main/java/com/woowacamp/android_accountbook_15/ui/tabs/setting/SettingFragment.kt
+++ app/src/main/java/com/woowacamp/android_accountbook_15/ui/tabs/setting/SettingFragment.kt
@@ -4,15 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.Composable
 import androidx.fragment.app.Fragment
+import com.woowacamp.android_accountbook_15.databinding.FragmentSettingBinding
+import com.woowacamp.android_accountbook_15.ui.theme.AndroidAccountBook15Theme
 
 class SettingFragment: Fragment() {
+
+    private var _binding: FragmentSettingBinding? = null
+    private val binding get() = requireNotNull(_binding)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        _binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.composeContainer.setContent {
+            AndroidAccountBook15Theme {
+                SettingView()
+            }
+        }
     }
 }
