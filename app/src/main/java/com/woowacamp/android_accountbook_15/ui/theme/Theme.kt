2acab392a3748414d609a00eb884e8f8b36210f7
rosf73 <alswlzhsclq@naver.com>
2022-07-27 23:38:38 +0900

    [FEAT] [Main] 공통 테마 글씨 색상 등 수정

--- app/src/main/java/com/woowacamp/android_accountbook_15/ui/theme/Theme.kt
+++ app/src/main/java/com/woowacamp/android_accountbook_15/ui/theme/Theme.kt
@@ -6,7 +6,7 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
+private val ColorPalette = darkColors(
     primary = LightPurple,
     primaryVariant = Purple,
     secondary = Yellow,
@@ -15,26 +15,6 @@ private val DarkColorPalette = darkColors(
     onPrimary = White,
     onSecondary = White,
     onBackground = Purple
-)
-
-private val LightColorPalette = lightColors(
-    primary = LightPurple,
-    primaryVariant = Purple,
-    secondary = Yellow,
-    secondaryVariant = Red,
-    background = OffWhite,
-    onPrimary = White,
-    onSecondary = White,
-    onBackground = Purple
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
@@ -42,14 +22,14 @@ fun AndroidAccountBook15Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
 
     MaterialTheme(
-        colors = colors,
+        colors = ColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
