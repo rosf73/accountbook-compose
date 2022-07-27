0d12f4148e1777ed8d1fe508f5ec07801d80c1bb
rosf73 <alswlzhsclq@naver.com>
2022-07-27 18:36:58 +0900

    [FEAT] 공식 문서의 안내에 따라 DAO 기본 정의

---
+++ app/src/main/java/com/woowacamp/android_accountbook_15/data/dao/HistoryDAO.kt
@@ -0,0 +1,19 @@
+package com.woowacamp.android_accountbook_15.data.dao
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.woowacamp.android_accountbook_15.utils.DATABASE_NAME
+import com.woowacamp.android_accountbook_15.utils.DATABASE_VERSION
+
+class HistoryDAO(context: Context)
+    : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+}
