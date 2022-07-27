67328f2cf2e4c5d134ef6ae36cd446bf316bfba1
rosf73 <alswlzhsclq@naver.com>
2022-07-27 18:18:40 +0900

    [FEAT] Model 정의

---
+++ app/src/main/java/com/woowacamp/android_accountbook_15/data/model/Category.kt
@@ -0,0 +1,8 @@
+package com.woowacamp.android_accountbook_15.data.model
+
+data class Category(
+    val id: Long,
+    val type: Int,
+    val name: String,
+    val color: String
+)
---
+++ app/src/main/java/com/woowacamp/android_accountbook_15/data/model/PaymentMethod.kt
@@ -0,0 +1,6 @@
+package com.woowacamp.android_accountbook_15.data.model
+
+data class PaymentMethod(
+    val id: Long,
+    val name: String
+)
