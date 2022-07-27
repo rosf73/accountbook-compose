e4d6b8e21ee25d2a33150a3968a8e87b02313733
rosf73 <alswlzhsclq@naver.com>
2022-07-27 17:10:40 +0900

    [CHORE] Database 정보를 local.properties에 정의

---
+++ app/src/main/java/com/woowacamp/android_accountbook_15/utils/BuildConfig.kt
@@ -0,0 +1,6 @@
+package com.woowacamp.android_accountbook_15.utils
+
+import com.woowacamp.android_accountbook_15.BuildConfig
+
+const val DATABASE_NAME = BuildConfig.DATABASE_NAME
+const val DATABASE_VERSION = BuildConfig.DATABASE_VERSION
