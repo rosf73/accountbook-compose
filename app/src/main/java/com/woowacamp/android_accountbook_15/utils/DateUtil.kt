98a3f36107e6f705617d90c79d4dbbb30714a3c5
rosf73 <alswlzhsclq@naver.com>
2022-07-28 22:59:41 +0900

    [FEAT] [History] 날짜 변환 메소드 정의

--- app/src/main/java/com/woowacamp/android_accountbook_15/utils/DateUtil.kt
+++ app/src/main/java/com/woowacamp/android_accountbook_15/utils/DateUtil.kt
@@ -8,3 +8,17 @@ fun getTodayMonthAndYear(): String {
     val today = Calendar.getInstance().time
     return SimpleDateFormat("yyyy년 MM월", Locale.KOREA).format(today)
 }
+
+fun getTodayMonthAndYear(year: Int, month: Int): String {
+    return "${year}년 ${month}월"
+}
+
+fun getTodayMonth(): Int {
+    val today = Calendar.getInstance().time
+    return SimpleDateFormat("MM", Locale.KOREA).format(today).toInt()
+}
+
+fun getTodayYear(): Int {
+    val today = Calendar.getInstance().time
+    return SimpleDateFormat("yyyy", Locale.KOREA).format(today).toInt()
+}
