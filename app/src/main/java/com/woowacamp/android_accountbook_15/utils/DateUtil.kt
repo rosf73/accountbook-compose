8037d2da4009d16b72ee4132a46f16488cbeed20
rosf73 <alswlzhsclq@naver.com>
2022-08-01 21:33:05 +0900

    [FEAT] Korean Date를 Hyphen Date로 변경하는 함수 구현

--- app/src/main/java/com/woowacamp/android_accountbook_15/utils/DateUtil.kt
+++ app/src/main/java/com/woowacamp/android_accountbook_15/utils/DateUtil.kt
@@ -58,3 +58,9 @@ fun getDaysInMonth(year: Int, month: Int): Int
         Calendar.FEBRUARY -> if (year % 4 == 0 && year % 100 != 0 && year % 400 == 0) 29 else 28
         else -> throw IllegalArgumentException("Invalid Month")
     }
+
+fun changeKoreanToHyphen(dayKorean: String): String {
+    val splitDate = dayKorean.split(". ")
+
+    return "${splitDate[0]}-${splitDate[1]}-${splitDate[2].split(" ")[0]}"
+}
