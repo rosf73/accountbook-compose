4da2aa7437c1493ac46c796754932c1347a5f24b
rosf73 <alswlzhsclq@naver.com>
2022-08-01 17:35:58 +0900

    [FEAT] 요일을 함께 가져오는 DateUtil 함수 정의

--- app/src/main/java/com/woowacamp/android_accountbook_15/utils/DateUtil.kt
+++ app/src/main/java/com/woowacamp/android_accountbook_15/utils/DateUtil.kt
@@ -16,3 +16,36 @@ fun getTodayYear(): Int {
     val today = Calendar.getInstance().time
     return SimpleDateFormat("yyyy", Locale.KOREA).format(today).toInt()
 }
+
+fun getTodayKorean(): String {
+    val today = Calendar.getInstance().time
+
+    var day = ""
+    when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
+        1 -> day = "일"
+        2 -> day = "월"
+        3 -> day = "화"
+        4 -> day = "수"
+        5 -> day = "목"
+        6 -> day = "금"
+        7 -> day = "토"
+    }
+    return SimpleDateFormat("yyyy. MM. dd ${day}요일", Locale.KOREA).format(today)
+}
+
+fun getDayKorean(year: Int, month: Int, date: Int): String {
+    val cal = Calendar.getInstance()
+    cal.set(year, month, date)
+
+    var day = ""
+    when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
+        1 -> day = "일"
+        2 -> day = "월"
+        3 -> day = "화"
+        4 -> day = "수"
+        5 -> day = "목"
+        6 -> day = "금"
+        7 -> day = "토"
+    }
+    return SimpleDateFormat("yyyy. MM. dd ${day}요일", Locale.KOREA).format(cal.time)
+}
