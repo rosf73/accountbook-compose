2eb8fdb96ce42caff515c8ec46b175da5cf3ac46
rosf73 <alswlzhsclq@naver.com>
2022-08-02 22:00:17 +0900

    [CHORE] 달력, 통계 화면 기본 세팅

---
+++ app/src/main/java/com/woowacamp/android_accountbook_15/ui/tabs/calendar/CalendarScreen.kt
@@ -0,0 +1,55 @@
+package com.woowacamp.android_accountbook_15.ui.tabs.calendar
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.painterResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.woowacamp.android_accountbook_15.R
+import com.woowacamp.android_accountbook_15.ui.components.DatePicker
+import com.woowacamp.android_accountbook_15.ui.components.Header
+import com.woowacamp.android_accountbook_15.ui.tabs.history.HistoryViewModel
+import com.woowacamp.android_accountbook_15.utils.getMonthAndYearKorean
+
+@Composable
+fun CalendarScreen(
+    viewModel: HistoryViewModel = hiltViewModel()
+) {
+    val year by viewModel.currentYear.collectAsState()
+    val month by viewModel.currentMonth.collectAsState()
+
+    val (isDateOpened, setDateOpened) = remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            Header(
+                title = getMonthAndYearKorean(year, month),
+                onTitleClick = { setDateOpened(true) },
+                leftIcon = painterResource(R.drawable.ic_left),
+                onLeftClick = {
+                    viewModel.setCurrentDate(
+                        if (month-1 > 0) year else year-1,
+                        if (month-1 > 0) month-1 else 12
+                    )
+                },
+                rightIcon = painterResource(R.drawable.ic_right),
+                onRightClick = {
+                    viewModel.setCurrentDate(
+                        if (month+1 > 12) year+1 else year,
+                        if (month+1 > 12) 1 else month+1
+                    )
+                })
+        }
+    ) {
+
+    }
+
+    if (isDateOpened) {
+        DatePicker(
+            initYear = year,
+            initMonth = month,
+            onOpen = setDateOpened,
+            onDateChanged = { newY, newM, _ ->
+                viewModel.setCurrentDate(newY, newM)
+            })
+    }
+}
--- app/src/main/java/com/woowacamp/android_accountbook_15/ui/tabs/graph/GraphFragment.kt
+++ app/src/main/java/com/woowacamp/android_accountbook_15/ui/tabs/graph/GraphFragment.kt
@@ -5,15 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.woowacamp.android_accountbook_15.R
+import com.woowacamp.android_accountbook_15.databinding.FragmentGraphBinding
+import com.woowacamp.android_accountbook_15.ui.tabs.history.HistoryViewModel
+import com.woowacamp.android_accountbook_15.ui.theme.AndroidAccountBook15Theme
 
 class GraphFragment : Fragment() {
+
+    private var _binding: FragmentGraphBinding? = null
+    private val binding: FragmentGraphBinding get() = requireNotNull(_binding)
+
+    private val viewModel: HistoryViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graph, container, false)
+    ): View {
+        _binding = FragmentGraphBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.composeGraph.setContent {
+            AndroidAccountBook15Theme {
+                GraphScreen(viewModel)
+            }
+        }
     }
 }
