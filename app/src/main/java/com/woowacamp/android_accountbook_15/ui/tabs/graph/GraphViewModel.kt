fdc7d47b8965159e9ab6e749f4f46f55409f7d9e
rosf73 <alswlzhsclq@naver.com>
2022-08-04 12:14:47 +0900

    [FEAT] [Graph] 카테고리 별 내역을 가져올 ViewModel 정의

---
+++ app/src/main/java/com/woowacamp/android_accountbook_15/ui/tabs/graph/GraphViewModel.kt
@@ -0,0 +1,23 @@
+package com.woowacamp.android_accountbook_15.ui.tabs.graph
+
+import androidx.lifecycle.ViewModel
+import com.woowacamp.android_accountbook_15.data.AccountBookRepository
+import com.woowacamp.android_accountbook_15.data.model.Category
+import com.woowacamp.android_accountbook_15.data.model.History
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class GraphViewModel @Inject constructor(
+    private val repository: AccountBookRepository
+): ViewModel() {
+
+    private val _historiesEachCategory = MutableStateFlow(listOf<History>())
+    val historiesEachCategory: StateFlow<List<History>> get() = _historiesEachCategory
+
+    fun setHistories(year: Int, month: Int, categoryId: Long) {
+        _historiesEachCategory.value = repository.readHistoriesEachCategory(year, month, categoryId).getOrThrow()
+    }
+}
